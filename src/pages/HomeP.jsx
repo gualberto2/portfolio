16007154389be464c2c5213ca98e4d774508bf5f
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-12 18:20:52 -0700

    working on my other housing cards

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -8,15 +8,12 @@ import { RxDoubleArrowDown, RxDoubleArrowRight } from "react-icons/rx";
 import React from "react";
 
 const Home = () => {
-  //
-  //
-  //
   return (
     <body className="px-2">
-      <section className="max-w-[95%] mx-auto xl:max-w-6xl">
-        <section className="grid grid-rows-2 grid-cols-1  ">
-          <div className=" flex flex-col pt-20 gap-8">
-            <h1 className=" flex flex-col text-center text-5xl md:text-6xl font-semibold tracking-tighter">
+      <section className="max-w-[95%] mx-auto 2xl:max-w-6xl">
+        <section className="">
+          <div className="py-20">
+            <h1 className=" flex flex-col text-center text-5xl md:text-6xl font-semibold tracking-tighter mb-8">
               <span>Hi. I'm Elijah. </span>
               <span>A Developer.</span>
             </h1>
@@ -36,6 +33,7 @@ const Home = () => {
                     1000,
                     "Driving others to acquire the same love for code",
                     1000,
+                    "Experience collaborating with other developers and clients",
                   ]}
                   wrapper="span"
                   speed={60}
@@ -92,8 +90,7 @@ const Home = () => {
         <section className="mt-16">
           <div>
             <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-5 md:grid-rows-3 ">
-              {/* Main card //// */}
-              <div className="bg-gradient-to-r from-[#88A0BF] to-[#7D9ABF] hover:from-[#7A95B8] hover:to-[#7A95B8] transition duration-150 ease-in-out transform p-12 md:p-0 md:text-center md:py-12 rounded-3xl m-2 col-start-1 col-end-3 row-start-1 row-end-2 md:col-start-3 md:col-end-4  md:row-start-1 md:row-end-2 cursor-pointer card">
+              <div className="bg-gradient-to-r from-[#88A0BF] to-[#7D9ABF] hover:from-[#7A95B8] hover:to-[#7A95B8] transition duration-150 ease-in-out transform p-12 md:p-0 md:text-center md:py-12 rounded-3xl m-2 col-start-1 col-end-3 row-start-1 row-end-2 md:col-start-1 md:col-end-3  md:row-start-1 md:row-end-2 cursor-pointer card z-20">
                 <h2 className="text-xl text-inherit font-normal uppercase">
                   Card
                 </h2>
@@ -101,15 +98,22 @@ const Home = () => {
                   Uno
                 </h1>
                 <RxDoubleArrowDown className="absolute inline-block md:hidden bottom-8 right-10 bounceEffect text-2xl icon-animation transition duration-200 ease-in-out" />
-                <RxDoubleArrowRight className="absolute hidden md:inline-block bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
+                <RxDoubleArrowRight className="absolute hidden md:inline-block slideEffect bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
               </div>
-              {/* Sub card 2 */}
-              <div className="bg-gradient-to-r from-[#A7C7C4] to-[#9FC6C2] hover:from-[#B4CFCD] hover:to-[#B4CFCD] p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform cursor-pointer  card col-start-1 col-end-3 row-start-2 row-end-3 md:col-start-3 md:col-end-4 md:row-start-2 md:row-end-3">
+              <div className="bg-white transition duration-150 ease-in-out transform p-12 md:p-0 md:text-center md:py-12 rounded-3xl m-2 col-start-1 col-end-3 row-start-1 row-end-2 md:col-start-2 md:col-end-7  md:row-start-1 md:row-end-2 cursor-pointer z-10">
+                <h2 className="text-xl text-inherit font-normal uppercase">
+                  Card
+                </h2>
+                <h1 className="text-4xl text-inherit font-medium uppercase">
+                  Uno
+                </h1>
+              </div>
+              <div className="bg-gradient-to-r from-[#A7C7C4] to-[#9FC6C2] hover:from-[#B4CFCD] hover:to-[#B4CFCD] p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform cursor-pointer  card col-start-1 col-end-3 row-start-2 row-end-3 md:col-start-2 md:col-end-4 md:row-start-2 md:row-end-3">
                 <h2 className="text-xl font-normal uppercase">Card</h2>
                 <h1 className="text-4xl font-medium uppercase">dos</h1>
                 <div className="arrow-container">
                   <RxDoubleArrowDown className="absolute inline-block md:hidden bottom-8 right-10 text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-                  <RxDoubleArrowRight className="absolute hidden md:inline-block bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
+                  <RxDoubleArrowRight className="absolute hidden md:inline-block slideEffect right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
                 </div>
                 <img src="" alt="" />
               </div>
@@ -118,7 +122,7 @@ const Home = () => {
                 <h1 className="text-4xl font-medium uppercase">tres</h1>
                 <div className="arrow-container">
                   <RxDoubleArrowDown className="absolute inline-block md:hidden bottom-8 right-10 text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-                  <RxDoubleArrowRight className="absolute hidden md:inline-block bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
+                  <RxDoubleArrowRight className="absolute hidden md:inline-block slideEffect bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
                 </div>
                 <img src="" alt="" />
               </div>
