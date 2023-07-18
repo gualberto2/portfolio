2c35617bd1b5966823f6f63231f50885d134f780
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-18 07:22:45 -0700

    styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="pt-20">
+      <h1 className="text-center text-5xl md:text-6xl font-semibold tracking-tighter mb-8">
+        I'm Elijah
+      </h1>
+      <div className="">
+        <div className="">
+          <h2>
+            I'm a Full-stack Software Developer working on the West Coast of the
+            United States
+          </h2>
+          <h3>
+            For over 1 year, I've worked in various areas of Full-stack
+            development, including front-end and back-end development, design,
+            marketing, UI/UX, Ruby and Rails and Node.JS. I'm proud to have worn
+            many hats.
+          </h3>
+          <h3 className=""></h3>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
