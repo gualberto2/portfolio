98959a63a0a8f5d7e84b0a2971f53a356cf16f6c
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-19 10:45:54 -0700

    half footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="grid grid-cols-8 mt-auto py-20">
-      <div className="md:col-start-2 md:col-end-4">
+    <footer className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl md:grid md:grid-cols-8 mt-auto py-20 ">
+      <div className="md:col-start-2 md:col-end-4 mb-8 md:mb-0">
         {/* on repeat spotify api thingy here */}
-        <p className="cursor-pointer">
-          <span>Elijah</span>
+        <div className="bg-green-100 rounded-3xl p-4 md:col-start-2 md:col-end-5">
+          <h3>Spotify</h3>
+          <h4>On Repeat</h4>
+        </div>
+        <p className="cursor-pointer flex flex-row justify-between">
+          <span>C 2023 Elijah</span>
           <span className="cursor-pointer">Colophon</span>
         </p>
       </div>
-      <section className="flex flex-row gap-8 px-12 md:col-start-5 md:col-end-8">
+      <section className="flex flex-row gap-8 px-12 md:col-start-5 md:col-end-8 justify-center align-center">
         <div className="flex flex-col">
           <h3 className="font-medium text-2xl mb-2">Elsewhere</h3>
           <div>
