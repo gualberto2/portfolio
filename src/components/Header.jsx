4678b96a96cdf8e8b69a1d667d536b3dc21dad71
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-19 09:08:02 -0700

    made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,26 +8,25 @@ const Header = () => {
     <nav className="flex flex-col md:flex-row justify-center items-center mt-2 pt-6 sticky">
       <ul className="flex flex-row items-center gap-10 md:mr-4">
         <li
-          className="font-medium text-xl cursor-pointer"
+          className="font-medium text-md md:text-xl cursor-pointer"
           onClick={() => navigate("/")}
         >
-          <span>Home</span>{" "}
-          <span className="border px-2 border-black font-normal">/</span>
+          Home
         </li>
         <li
-          className="font-medium text-xl cursor-pointer"
+          className="font-medium text-md md:text-xl cursor-pointer"
           onClick={() => navigate("/resume")}
         >
           Resume
         </li>
         <li
-          className="font-medium text-xl cursor-pointer"
+          className="font-medium text-md md:text-xl cursor-pointer"
           onClick={() => navigate("/about")}
         >
           About
         </li>
         <li
-          className="font-medium text-xl cursor-pointer"
+          className="font-medium text-md md:text-xl cursor-pointer"
           onClick={() => navigate("/work")}
         >
           Work
