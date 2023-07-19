1f1dc9bfd9368f5ab8955d56bc6759e06e2fe2c8
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-19 09:04:06 -0700

    styling refactor

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -1,26 +1,40 @@
-import { FaAngellist, FaRust, FaPeopleCarry } from "react-icons/fa";
+import {
+  FaAngellist,
+  FaRust,
+  FaPeopleCarry,
+  FaCss3Alt,
+  FaHtml5,
+  FaNpm,
+} from "react-icons/fa";
 import { BsBoxArrowInRight } from "react-icons/bs";
 import {
   BiCodeBlock,
   BiLogoReact,
+  BiLogoRedux,
   BiLogoTailwindCss,
   BiLogoJavascript,
   BiLogoMongodb,
   BiLogoFirebase,
+  BiLogoGraphql,
+  BiLogoGithub,
+  BiLogoGit,
 } from "react-icons/bi";
 import { serverTimestamp, addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { DiRubyRough } from "react-icons/di";
-import { SiRubyonrails } from "react-icons/si";
+import { LiaNode } from "react-icons/lia";
+import { SiRubyonrails, SiStrapi, SiPostgresql } from "react-icons/si";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-// import { RxDoubleArrowDown, RxDoubleArrowUp } from "react-icons/rx";
+import Loading from "../components/Loading";
 import React, { useState } from "react";
 import PopUpMsg from "../components/PopUpMsg";
-import Loading from "../components/Loading";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   const [showPopup, setShowPopup] = useState(true);
   const [loading, setLoading] = useState(false);
 
@@ -63,27 +77,12 @@ const Home = () => {
   }
 
   if (loading) return <Loading />;
-  // const [showFrontEnd, setShowFrontEnd] = useState(false);
-  // const [showBackEnd, setShowBackEnd] = useState(false);
-  // const [showOOP, setShowOOP] = useState(false);
-
-  // const handleShowFrontEnd = () => {
-  //   setShowFrontEnd(!showFrontEnd);
-  // };
-
-  // const handleShowBackEnd = () => {
-  //   setShowBackEnd(!showBackEnd);
-  // };
-
-  // const handleShowOOP = () => {
-  //   setShowOOP(!showOOP);
-  // };
 
   return (
     <body className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl">
       {showPopup && <PopUpMsg isOpen={true} closeModal={closeModal} />}
 
-      <section className="">
+      <section className="mb-16">
         <section className="">
           <div className="pt-20 ">
             <h1 className=" flex flex-col text-center text-5xl md:text-6xl font-semibold tracking-tighter mb-8">
@@ -106,7 +105,8 @@ const Home = () => {
                     1000,
                     "Driving others to acquire the same love for code",
                     1000,
-                    "Experience collaborating with other developers and clients",
+                    "Experience collaborating with other developers and clients.",
+                    1000,
                   ]}
                   wrapper="span"
                   speed={60}
@@ -118,7 +118,10 @@ const Home = () => {
           <div>
             <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-4 md:grid-rows-2 ">
               {/* Main card //// */}
-              <div className=" bg-gradient-to-r from-[#C2B575] to-[#C5B667] hover:from-[#B8AF7A] hover:to-[#B8AF7A] transition duration-150 ease-in-out transform p-12 rounded-3xl m-2 col-start-1 col-end-3 row-start-1 row-end-3 md:col-start-1 md:col-end-3  md:row-start-1 md:row-end-3 cursor-pointer card">
+              <div
+                className=" bg-gradient-to-r from-[#C2B575] to-[#C5B667] hover:from-[#B8AF7A] hover:to-[#B8AF7A] transition duration-150 ease-in-out transform p-12 rounded-3xl m-2 col-start-1 col-end-3 row-start-1 row-end-3 md:col-start-1 md:col-end-3  md:row-start-1 md:row-end-3 cursor-pointer card"
+                onClick={() => navigate("/biggest-project")}
+              >
                 <h2 className="text-xl text-inherit font-normal uppercase">
                   My Biggest
                 </h2>
@@ -128,7 +131,10 @@ const Home = () => {
                 <BiCodeBlock className="absolute bottom-8 right-10 text-2xl icon-animation transition duration-200 ease-in-out" />
               </div>
               {/* Sub card 2 */}
-              <div className="bg-gradient-to-r from-[#A7C7C4] to-[#9FC6C2] hover:from-[#B4CFCD] hover:to-[#B4CFCD] p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform cursor-pointer  card col-start-1 col-end-3 row-start-3 row-end-4 md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-2">
+              <div
+                className="bg-gradient-to-r from-[#A7C7C4] to-[#9FC6C2] hover:from-[#B4CFCD] hover:to-[#B4CFCD] p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform cursor-pointer  card col-start-1 col-end-3 row-start-3 row-end-4 md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-2"
+                onClick={() => navigate("/projects")}
+              >
                 <h2 className="text-xl font-normal uppercase">View my</h2>
                 <h1 className="text-4xl font-medium uppercase">Work</h1>
                 <div className="arrow-container">
@@ -137,14 +143,20 @@ const Home = () => {
                 <img src="" alt="" />
               </div>
               {/* Sub card 3 */}
-              <div className="bg-[#60AB86] hover:bg-[#6CB28F] bg-gradient-to-r p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform  cursor-pointer card col-start-1 col-end-3 row-start-4 row-end-5 md:col-start-4 md:col-end-5 md:row-start-1 md:row-end-2">
+              <div
+                className="bg-[#60AB86] hover:bg-[#6CB28F] bg-gradient-to-r p-12 rounded-3xl m-2 transition duration-150 ease-in-out transform  cursor-pointer card col-start-1 col-end-3 row-start-4 row-end-5 md:col-start-4 md:col-end-5 md:row-start-1 md:row-end-2"
+                onClick={() => navigate("/about")}
+              >
                 <h2 className="text-xl font-normal uppercase">All About</h2>
                 <h1 className="text-4xl font-medium uppercase">me</h1>
                 <FaAngellist className="absolute bottom-8 icon-animation right-10 text-2xl  transition duration-200 ease-in-out" />
                 <img src="" alt="" />
               </div>
               {/* SUb card 4 */}
-              <div className="bg-[#695958] hover:bg-[#7A6766] p-12 rounded-3xl m-2 md:col-start-3 md:col-end-5 md:row-start-2 md:row-end-3 transition duration-150 ease-in-out transform  cursor-pointer card col-start-1 col-end-3 row-start-5 row-end-6">
+              <div
+                className="bg-[#695958] hover:bg-[#7A6766] p-12 rounded-3xl m-2 md:col-start-3 md:col-end-5 md:row-start-2 md:row-end-3 transition duration-150 ease-in-out transform  cursor-pointer card col-start-1 col-end-3 row-start-5 row-end-6"
+                onClick={() => navigate("/contact")}
+              >
                 <h2 className="text-xl font-normal uppercase text-white">
                   Get in
                 </h2>
@@ -161,133 +173,156 @@ const Home = () => {
         <div className="flex justify-center my-4">
           <MdOutlineKeyboardArrowDown className="text-3xl text-center bounceEffect" />
         </div>
-        {/* <div className="hidden md:inline-block">
-          <HomeCardsLarge />
-        </div>
-        <div className="inline-block w-full md:hidden">
-          <HomeCardsMobile />
-        </div> */}
       </section>
-      <section className="my-12 grid grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 justify-center items-center gap-4 md:gap-6">
-        {/* <div className="text-center bg-gradient-to-r from-[#88A0BF] to-[#7D9ABF] hover:from-[#7A95B8] hover:to-[#7A95B8] p-12 rounded-3xl transition ease-in-out duration-200 hover:shadow-md">
-          <h3 className="text-2xl mb-2 font-semibold">Front-End</h3>
-          <div className="flex flex-row text-5xl justify-center mb-3">
-            <BiLogoReact />
-            <BiLogoJavascript />
-            <BiLogoTailwindCss />
+
+      <section className="m-2 px-8 py-12 bg-gradient-to-r from-[#88A0BF] to-[#7D9ABF] rounded-3xl grid md:grid-cols-10">
+        <div className="md:col-start-2 md:col-end-3 flex flex-col justify-center mb-4">
+          <h3 className="text-xl font-normal uppercase ">My</h3>
+          <h2 className="text-4xl font-medium uppercase ">Skills</h2>
+        </div>
+
+        <div className="md:col-start-5 md:col-end-11 flex flex-wrap justify-center text-6xl gap-8">
+          <div className="language-container">
+            <BiLogoFirebase
+              className="language-icon text-grow-on-hover "
+              title="Firebase"
+            />
+            <div className="language-text">Firebase</div>
           </div>
-          <div className="border-t border-black px-8 pt-1">
-            <p
-              className={`tracking-widest text-lg ${
-                showMore ? "line-clamp-none" : "line-clamp-3"
-              }`}
-            >
-              Experienced in high demand Front-End languages including HTML,
-              CSS, JS, React and Next. Highly adaptable and flexible with
-              learning and utilizing newer and older languages
-            </p>
-            {!showMore && (
-              <button
-                className="text-black hover:underline mt-2"
-                onClick={handleShowMore}
-              >
-                Show More
-              </button>
-            )}
+
+          <div className="language-container">
+            <BiLogoMongodb
+              className="language-icon text-grow-on-hover"
+              title="MongoDB"
+            />
+            <div className="language-text">MongoDB</div>
           </div>
-        </div> */}
-        {/* ... */}
+
+          <div className="language-container">
+            <SiRubyonrails
+              className="language-icon text-grow-on-hover"
+              title="Ruby on Rails"
+            />
+            <div className="language-text">Ruby on Rails</div>
+          </div>
+
+          <div className="language-container">
+            <SiStrapi
+              className="language-icon text-grow-on-hover"
+              title="Strapi"
+            />
+            <div className="language-text">Strapi</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoReact
+              className="language-icon text-grow-on-hover"
+              title="React"
+            />
+            <div className="language-text">React</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoGit
+              className="language-icon text-grow-on-hover"
+              title="Git"
+            />
+            <div className="language-text">Git</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoGraphql
+              className="language-icon text-grow-on-hover"
+              title="GraphQL"
+            />
+            <div className="language-text">GraphQL</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoJavascript
+              className="language-icon text-grow-on-hover"
+              title="JavaScript"
+            />
+            <div className="language-text">JavaScript</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoTailwindCss
+              className="language-icon text-grow-on-hover"
+              title="Tailwind CSS"
+            />
+            <div className="language-text">Tailwind CSS</div>
+          </div>
+
+          <div className="language-container">
+            <DiRubyRough
+              className="language-icon text-grow-on-hover"
+              title="Ruby"
+            />
+            <div className="language-text">Ruby</div>
+          </div>
+
+          <div className="language-container">
+            <FaRust className="language-icon text-grow-on-hover" title="Rust" />
+            <div className="language-text">Rust</div>
+          </div>
+
+          <div className="language-container">
+            <LiaNode
+              className="language-icon text-grow-on-hover"
+              title="Node.js"
+            />
+            <div className="language-text">Node.js</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoGithub
+              className="language-icon text-grow-on-hover"
+              title="GitHub"
+            />
+            <div className="language-text">GitHub</div>
+          </div>
+
+          <div className="language-container">
+            <FaHtml5
+              className="language-icon text-grow-on-hover"
+              title="HTML5"
+            />
+            <div className="language-text">HTML5</div>
+          </div>
+
+          <div className="language-container">
+            <BiLogoRedux
+              className="language-icon text-grow-on-hover"
+              title="Redux"
+            />
+            <div className="language-text">Redux</div>
+          </div>
+
+          <div className="language-container">
+            <FaNpm className="language-icon text-grow-on-hover" title="npm" />
+            <div className="language-text">npm</div>
+          </div>
+
+          <div className="language-container">
+            <SiPostgresql
+              className="language-icon text-grow-on-hover"
+              title="PostgreSQL"
+            />
+            <div className="language-text">PostgreSQL</div>
+          </div>
+
+          <div className="language-container">
+            <FaCss3Alt
+              className="language-icon text-grow-on-hover"
+              title="CSS3"
+            />
+            <div className="language-text">CSS3</div>
+          </div>
+        </div>
       </section>
-      <section className="min-h-[420px] grid grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1  gap-4 md:gap-6">
-        <div className="bg-gradient-to-r from-[#88A0BF] to-[#7D9ABF]   rounded-3xl transition ease-in-out duration-200 hover:shadow-md">
-          <div className="px-12 pt-12 pb-2">
-            <div className="flex flex-row text-2xl font-medium uppercase mb-2">
-              <BiLogoReact />
-              <BiLogoJavascript />
-              <BiLogoTailwindCss />
-            </div>
-            <h3 className="text-4xl font-medium uppercase">Front-End</h3>
-          </div>
-          <div className=" px-8 pt-1">
-            <p
-              // className={`tracking-widest text-lg ${
-              //   showFrontEnd ? "line-clamp-none" : "line-clamp-3"
-              // }`}
-              className="tracking-widest text-lg pb-6"
-            >
-              Experienced in high demand Front-End languages including HTML,
-              CSS, JS, React and Next. Highly adaptable and flexible with
-              learning and utilizing newer and older languages
-            </p>
-            {/* <button onClick={() => handleShowFrontEnd()}>
-              {(showFrontEnd && (
-                <RxDoubleArrowUp className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )) || (
-                <RxDoubleArrowDown className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )}
-            </button> */}
-          </div>
-        </div>
-        <div className="  rounded-3xl bg-gradient-to-r from-[#AAA1CE] to-[#A399CC] transition ease-in-out duration-200 hover:shadow-md">
-          <div className="px-12 pt-12 pb-2">
-            <div className="flex flex-row text-2xl font-medium uppercase mb-2">
-              <BiLogoFirebase />
-              <BiLogoMongodb />
-              <SiRubyonrails />
-            </div>
-            <h3 className="text-4xl font-medium uppercase">Back-End</h3>
-          </div>
-          <div className="px-8 pt-1">
-            <p
-              // className={`tracking-widest text-lg ${
-              //   showBackEnd ? "line-clamp-none" : "line-clamp-3"
-              // }`}
-              className="tracking-widest text-lg pb-6"
-            >
-              Whether simplicity or complexity, my expertise allows for robust
-              and data management, intergration and efficient development all
-              while delivering outstanding solutions with precision
-            </p>
-            {/* <button onClick={() => handleShowBackEnd()}>
-              {(showBackEnd && (
-                <RxDoubleArrowUp className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )) || (
-                <RxDoubleArrowDown className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )}
-            </button> */}
-          </div>
-        </div>
-        <div className="rounded-3xl bg-gradient-to-r from-[#B09396] to-[#B8999C]  transition ease-in-out duration-200 hover:shadow-md ">
-          <div className="px-12 pt-12 pb-2">
-            <div className="flex flex-row text-2xl mb-2">
-              <DiRubyRough />
-              <FaRust />
-            </div>{" "}
-            <h3 className="text-4xl font-medium uppercase">OOP</h3>
-          </div>
-          <div className="px-8 pt-1">
-            <p
-              // className={`tracking-widest text-lg ${
-              //   showOOP ? "line-clamp-none" : "line-clamp-3"
-              // }`}
-              className="tracking-widest text-lg pb-6"
-            >
-              Being new, using object-oriented programming (OOP) languages
-              offers clear syntax, powerful abstractions, and vast communities
-              for support, facilitating my learning journey and enabling
-              efficient problem-solving.
-            </p>
-            {/* <button onClick={() => handleShowOOP()}>
-              {(showOOP && (
-                <RxDoubleArrowUp className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )) || (
-                <RxDoubleArrowDown className="text-2xl bounceEffect icon-animation transition duration-200 ease-in-out" />
-              )}
-            </button> */}
-          </div>
-        </div>
-      </section>
-      <section className="lg:mx-[15%] my-12 bg-gradient-to-r from-[#C26697] to-[#C2709C] transition duration-150 ease-in-out p-8 rounded-2xl">
+
+      <section className="lg:mx-[15%] my-16 bg-gradient-to-r from-[#C26697] to-[#C2709C] transition duration-150 ease-in-out p-8 rounded-2xl">
         <div className="mb-8">
           <h2 className="text-xl font-normal uppercase">Lets work</h2>
           <h3 className="text-4xl text-inherit font-medium uppercase ">
@@ -331,7 +366,7 @@ const Home = () => {
               value={txtAr}
               min={15}
               max={500}
-              rows="3"
+              rows="1"
               className="md:row-start-2 md:row-end-3 md:col-span-2 px-6 md:mx-8 py-3 transition duration-150 ease-in-out  rounded-xl bg-[#C87EA6] hover:bg-[#B87A9B] placeholder-white row-start-4 row-end-6 "
             />
             <button
