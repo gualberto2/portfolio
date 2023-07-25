3e5e0f678ca2653a8a1cd471a03fba56374a777e
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-25 08:17:09 -0700

    starting section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl py-20 min-h-screen"></section>
+  );
 };
 
 export default Projects;
