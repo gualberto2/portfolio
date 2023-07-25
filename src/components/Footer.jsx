81c31850e5f921d4b9e92c5a6bac4042e9bd08cd
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-25 08:16:57 -0700

    removed bottom sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,8 @@ import React from "react";
 import { FaSpotify } from "react-icons/fa";
 
 const Footer = () => {
-  // const CLIENT_ID = "ced6aca9c9dc448799fc5a7159f99665";
-  // const CLIENT_SECRET = "afe87a2459df441ab4fc5a4d68018e5c";
-
-  // useEffect(() => {
-  //   //API access tokens
-  //   var authParameters = {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //       body:
-  //         "grant_type=client_credentials&client_id=" +
-  //         CLIENT_ID +
-  //         "&client_secret" +
-  //         CLIENT_SECRET,
-  //     },
-  //   };
-  //   fetch("https://accounts.spotify.com/api/token", authParameters)
-  //     .then((result) => result.json())
-  //     .then((data) => console.log(data));
-  // }, []);
   return (
-    <footer className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl md:grid md:grid-cols-8 mt-auto py-20 gap-6">
+    <footer className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl md:grid md:grid-cols-8 mt-auto py-20 gap-6 ">
       <div className="md:col-start-1 md:col-end-5 mb-8 md:mb-0 bg-green-800 p-6 mx-4 rounded-3xl relative">
         <h2 className="text-xl font-medium text-white">Spotify</h2>
         <h3 className="text-xl font-medium text-white mb-2">On Repeat</h3>
