22fc4046bad151750a0cf555ba140a759c9de4e5
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-26 07:05:50 -0700

    query hook created and written out

---
+++ src/hooks/useQueryPosts.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const useQueryPosts = ({ query, limit = 6 }) => {
+  const { slug } = useParams();
+  const { search } = useLocation();
+
+  const page = Number(new URLSearchParams(search).get("page")) || 1;
+  const skip = (page - 1) * limit;
+
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const { setLoading, setTotalPage } = useMyContext();
+
+  useEffect(() => {
+    setLoading(true);
+    graphcms
+      .request(query, { slug, limit, skip })
+      .then((res) => {
+        const count = res?.countConnection?.aggregate?.count || 0;
+        setTotalPage(Math.ceil(count / limit));
+        setPosts(res.posts);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setPosts([]);
+      })
+      .finally(() => setLoading(false));
+  }, [query, slug, limit, skip]);
+
+  return { posts, error };
+};
+
+export default useQueryPosts;
