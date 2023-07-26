585088f13c27b2ba9884ff0bef0a6c9409081fa3
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-26 07:04:51 -0700

    removed

--- src/pages/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Projects = () => {
-  return (
-    <section className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl py-20 min-h-screen"></section>
-  );
-};
-
-export default Projects;
