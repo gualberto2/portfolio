9e1955e7dd1f838fa41fcd3e047e971822b379d1
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-26 07:05:57 -0700

    starting work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <section className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl py-20 min-h-screen">
+      Projects
+    </section>
+  );
 };
 
 export default Work;
