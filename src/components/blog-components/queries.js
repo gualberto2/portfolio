a390202d711a9f6439f11dd2dbaa93d52bc08c7b
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-28 18:53:56 -0700

    edited some queries

--- src/components/blog-components/queries.js
+++ src/components/blog-components/queries.js
@@ -3,25 +3,53 @@ import { GraphQLClient, gql } from "graphql-request";
 export const graphcms = new GraphQLClient(process.env.REACT_APP_API);
 
 const post = `
-featuredImage {
-  url
-}
-content {
-  html
-}
-title
-slug
-id
-createdAt
-updatedAt
-excerpt
+    featuredImage {
+      url
+    }
+    content {
+      html
+    }
+
+    title
+    slug
+    id
+    createdAt
+    updatedAt
+    excerpt
+
 `;
-const category = `
-id
-title
-slug`;
+
 const comment = `
 id
 name
 email
 comment`;
+
+const category = `
+    id,
+    slug,
+    title
+`;
+
+export const QUERY_POSTS = gql`
+{
+    posts(){
+        ${post}
+        categories(){
+            ${category}
+        }
+    }
+  }
+
+`;
+
+export const QUERY_ONE_POST = gql`
+query GetOnePost($slug: String!) {
+    posts(where: {slug: $slug}){
+        ${post}
+        comments(){
+            ${comment}
+        }
+    }
+}
+`;
