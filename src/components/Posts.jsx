a33258a62ec1c95765fdcbe1f8cf9a1b3f7a7a08
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-01 15:39:33 -0700

    trying 3 different cards

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -11,13 +11,16 @@ const Posts = ({ posts }) => {
       {posts?.map((post) => (
         <div
           key={post.id}
-          className="bg-[#E0E0E0] rounded-3xl p-5  grid grid-cols-4 md:grid-cols-8 grid-rows-2 md:grid-rows-1"
+          className="bg-[#CCCCCC] hover:greenTextLinks transition ease-in-out duration-200  rounded-3xl p-5  grid grid-cols-4 md:grid-cols-8  md:grid-rows-1 w-full postLink"
         >
+          <div>
+            <p></p>
+          </div>
           <div
-            className="md:col-start-2 md:col-end-3 col-start-1 col-end-5 row-start-1 row-end-2 mb-1"
+            className="relative md:col-start-2 md:col-end-3 col-start-1 col-end-5 row-start-1 row-end-2 mb-1"
             onClick={() => navigate(`/article/${post.slug}`)}
           >
-            <h2 className="text-xl text-inherit font-normal uppercase cursor-pointer ">
+            <h2 className="text-xl text-inherit font-normal uppercase  ">
               {post.subTitle}
             </h2>
             <h2
@@ -26,32 +29,86 @@ const Posts = ({ posts }) => {
             >
               {post.title}
             </h2>
-            <p className="font-light text-md">
-              {" "}
-              {moment(post.createdAt).format("MM.DD.YY")}
-            </p>
+
             <div className="flex flex-row justify-around py-4 text-xl font-medium uppercase ">
               <p
                 onClick={() => (window.location.href = post.github)}
-                className="hover:text-gray-500 cursor-pointer transition duration-200 ease-in-out"
+                className="hover:underline cursor-pointer text-green-700 transition duration-200 ease-in-out"
               >
                 Github
               </p>
               <p
                 onClick={() => (window.location.href = post.liveSite)}
-                className="hover:text-gray-500 cursor-pointer transition duration-200 ease-in-out"
+                className=" hover:underline cursor-pointer text-green-700 transition duration-200 ease-in-out"
               >
                 Live
               </p>
             </div>
+            <img
+              src={post.featuredImage.url}
+              loading="lazy"
+              alt=""
+              className=" h-[200px] cursor-pointer w-full object-cover hover:scale-105 transition-scale duration-200 ease-in left-0 bottom-0 rounded-2xl card"
+            />
           </div>
-          <div className="col-start-1 col-end-5 bg-green-700 flex justify-center items-center rounded-2xl ">
-            <p className=" text-xl font-medium tracking-widest text-white p-6">
-              {post.excerpt}
-            </p>
+          {/* <div className="col-start-1 col-end-5 text-md font-normal tracking-widest">
+            <p>{post.shortDescriptionOfTheWebsite}</p>
+          </div> */}
+        </div>
+      ))}
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <div className="">
+            <div className="bg-[#a07178] p-12 rounded-3xl">
+              <h1 className="text-3xl">{post.title}</h1>
+              <h2>{post.shortDescriptionOfTheWebsite}</h2>
+              <ul className="grid grid-cols-8 my-3">
+                <li className="col-start-1 col-end-2">Github</li>
+                <li className="col-start-3 col-end-4">Live</li>
+                <li className="col-start-6 col-end-8 whitespace-nowrap">
+                  Learn More
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       ))}
+      <div className="flex flex-col bg-white rounded-lg p-8 shadow-md">
+        <div className="flex items-center justify-center bg-indigo-500 rounded-full h-12 w-12">
+          <svg
+            className="w-6 h-6 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            ></path>
+          </svg>
+        </div>
+        <h3 className="text-xl font-semibold mt-4">Help Scout AI</h3>
+        <p className="text-gray-600 mt-2">
+          Empowering customer support with AI-powered solutions for faster and
+          smarter responses.
+        </p>
+        <div className="flex justify-between items-center mt-4">
+          <a
+            href="https://www.helpscout.com/ai/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-indigo-600 hover:underline"
+          >
+            Learn More
+          </a>
+          <div className="flex items-center">
+            <span className="text-gray-600">5 mins read</span>
+            <span className="text-indigo-500 ml-2">→</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
