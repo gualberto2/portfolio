2144004742441189ad889749f88c0e5aedabf84d
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-01 15:39:24 -0700

    added short desc to query

--- src/components/blog-components/queries.js
+++ src/components/blog-components/queries.js
@@ -3,6 +3,7 @@ import { GraphQLClient, gql } from "graphql-request";
 export const graphcms = new GraphQLClient(process.env.REACT_APP_API);
 
 const post = `
+shortDescriptionOfTheWebsite
     featuredImage {
       url
     }
