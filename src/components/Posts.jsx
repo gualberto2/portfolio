3d68c6d53a4427d5abe31a1a02b9a258cfd4075e
gualberto2 <elijahwilsonnn@gmailcom>
2023-07-31 23:51:11 -0700

    messing around with styling, hard time

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,6 @@ import moment from "moment";
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Posts = ({ posts }) => {
   const navigate = useNavigate();
@@ -12,27 +11,30 @@ const Posts = ({ posts }) => {
       {posts?.map((post) => (
         <div
           key={post.id}
-          className="bg-[#C7DBEB] rounded-3xl p-5  grid grid-cols-4 md:grid-cols-8"
+          className="bg-[#C7DBEB] rounded-3xl p-5  grid grid-cols-4 md:grid-cols-8 grid-rows-2 md:grid-rows-1"
         >
-          <div className="md:col-start-2 md:col-end-3 col-start-1 col-end-3">
+          <div
+            className="md:col-start-2 md:col-end-3 col-start-1 col-end-5 row-start-1 row-end-2 mb-1"
+            onClick={() => navigate(`/article/${post.slug}`)}
+          >
+            <h2 className="text-xl text-inherit font-normal uppercase ">
+              {post.subTitle}
+            </h2>
             <h2
-              className="text-3xl font-medium flex flex-row items-center gap-2 hover:bg-red-300/70 rounded-xl"
+              className="text-4xl text-inherit font-medium uppercase"
               onClick={() => navigate(`/article/${post.slug}`)}
             >
               {post.title}
-              <FaExternalLinkAlt className="text-lg font-medium " />
             </h2>
-            <h3 className="text-xl font-light">
-              {moment(post.createdAt).format("MM/DD/YYYY")}
-            </h3>
-            <p onClick={() => (window.location.href = post.liveSite)}>Live</p>
-            <p onClick={() => (window.location.href = post.github)}>Github</p>
+            <p className="font-light text-md">
+              {" "}
+              {moment(post.createdAt).format("MM.DD.YY")}
+            </p>
           </div>
-          <div className="md:col-start-4 md:col-end-8 col-start-3 col-end-5">
-            <p className="tracking-widest text-md text-gray-900">
+          <div className="col-start-1 col-end-5">
+            <p className=" text-xl font-normal tracking-widest">
               {post.excerpt}
             </p>
-            <p>{post.shortDescriptionOfTheWebsite}</p>
           </div>
         </div>
       ))}
