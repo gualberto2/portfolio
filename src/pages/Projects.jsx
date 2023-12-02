77cabf51abf284efc13e074e520736b75e66c86f
gualberto2 <elijahwilsonnn@gmail.com>
2023-12-01 18:29:04 -0800

    MAde it look somewhat pretty

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,14 +9,17 @@ const Projects = () => {
     graphcms.request(QUERY_POSTS).then((res) => setPosts(res.posts));
   });
   return (
-    <section className="px-2 max-w-[95%] mx-auto 2xl:max-w-6xl py-20 min-h-screen">
-      <h1 className="text-center text-5xl md:text-6xl font-semibold tracking-tighter mb-8">
-        Projects
-      </h1>
-      <h2 className="text-center tracking-widest text-lg">
-        Discover the inner workings of my mind and gain insight into my creative
-        process.
-      </h2>
+    <section className="px-2 max-w-[95%] mx-auto lg:max-w-4xl py-20 min-h-screen">
+      {/* hdrAnim here */}
+      <div className="">
+        <h1 className="text-center text-5xl md:text-6xl font-semibold tracking-tighter mb-8">
+          Projects
+        </h1>
+        <h2 className="text-center tracking-widest text-lg">
+          Discover the inner workings of my mind and gain insight into my
+          creative process.
+        </h2>
+      </div>
 
       <div className="my-12">
         <h1 className="text-center font-light text-gray-800 text-md mb-3">
