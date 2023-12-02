c360a24bb26fb0203569d6132f893aa4b9baba36
gualberto2 <elijahwilsonnn@gmail.com>
2023-12-01 18:26:23 -0800

    added nice lil navbar scroll anim

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementById("navbar");
+      if (window.scrollY > 0) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    };
+
+    // Add the event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove the event listener on cleanup
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className=" mt-2 pt-6 sticky">
-      <ul className="grid grid-cols-4 md:flex md:justify-between md:mx-16 items-center">
-        <li
+    <nav className="flex justify-center">
+      <div
+        className=" mt-2 py-4 rounded-2xl px-12 sticky w-full flex justify-between  max-w-xl items-center"
+        id="navbar"
+      >
+        <p
           className="font-medium text-center col-start-1 col-end-2 text-md md:text-xl cursor-pointer"
           onClick={() => navigate("/")}
         >
           Home
-        </li>
-        <li
+        </p>
+        <p
           className="font-medium col-start-2 col-end-3 text-md md:text-xl cursor-pointer text-center"
           onClick={() => navigate("/resume")}
         >
           Resume
-        </li>
-        <li
+        </p>
+        <p
           className="font-medium col-start-3 col-end-4 text-md md:text-xl cursor-pointer text-center"
           onClick={() => navigate("/about")}
         >
           About
-        </li>
-        <li
+        </p>
+        <p
           className="font-medium col-start-4 col-end-5 text-md md:text-xl cursor-pointer text-center"
           onClick={() => navigate("/projects")}
         >
           Projects
-        </li>
-      </ul>
+        </p>
+      </div>
     </nav>
   );
 };
